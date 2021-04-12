f839b71e8fd32e187789cd130f1ff58c69b209f3
jan <jankretschmann1@gmail.com>
2021-04-12 15:38:29 -0600

    fixed bug with wrong distance matrix

--- math/emd.go
+++ math/emd.go
@@ -1,45 +1,25 @@
 package math
 
 import (
+	"log"
 
 	"earth-movers-backend/tools"
 )
 
-
 func emd(dist1 []int, dist2 []int) int {
-	// Only support same length distributions for now
-	/*
-	length := 0
-	if len(dist1) > len(dist2) { 
-		length = len(dist1)
-	} else {
-		length = len(dist2)
-	}
-	*/
-
 	length := len(dist1)
 	dif := make([]int, length)
 	result := 0
 
-	i := 0
-	for i < length {
+	for i := 0; i < length; i++ {
 		dif[i] = dist1[i] - dist2[i]
-		j := 0
-		for j < i {
-			result += tools.Abs(tools.Sum(dif))
-			j++
-		}
-		i++
+		result += tools.Abs(tools.Sum(dif[:i]))
 	}
 	return result
 }
 
 func DistanceMatrix(distributions [][]int, maxVal float64) [][]float64 {
 	distance_matrix := make([][]float64, len(distributions))
-
-	// Have to make one run through it here for the sym. init.
-	// of the lower triangular matrix
-	// Should prob be optimized
 	for i := range distributions {
 		distance_matrix[i] = make([]float64, len(distributions))
 	}
